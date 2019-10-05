a814398e0854244e2dc93860e40cfe083e9e843a
YeeLengLor <55327087+YeeLengLor@users.noreply.github.com>
2019-10-05 10:40:32 -0500

    Create mathematicalOperatorsDebug.py

---
+++ mathematicalOperatorsDebug.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+import string
+
+40 * 0 == -1 '40 * 0 = 0'
+
+“Hello 1 2 and” + 3 ' you can't add an int and string
+
+6 _5 _1= 31 '6*5+1=31'
+
+print(10 + “/” + 2) 'print(10/2) remove quotations'
+
+print(“LOL” * 3) ? 'its fine'   
+
+Print( “1” + “2” + “3”) 'p in print cant be capitalized'
+
+print(10 _ 2 == 0)  'there is no remainder in 10%2, so it equals zero'
