2702cafb63b7b0c8c01d0520bd1ce66b4518cdde
YeeLengLor <55327087+YeeLengLor@users.noreply.github.com>
2019-10-05 10:24:12 -0500

    Initial commit

---
+++ mathOperatorDrill.py
@@ -0,0 +1,65 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+'''Addition'''
+
+a = 2 +1
+
+a = 4
+
+b = 6
+
+c = a + b
+
+'''subtraction'''
+
+c = 3
+
+b = 5
+
+d = 2-1
+
+d = b - 5
+
+'''multiplication'''
+
+a = 3
+
+b = 6
+
+d = 4 * 5
+
+d = a * b
+
+'''division(float)'''
+
+a = 10
+
+b = 2
+
+d = 20 / 5
+
+d = a / b
+
+'''division(floor)'''
+
+a = 9
+
+b = 5
+
+d = 8 // 3
+
+d = b // a
+
+'''modulus'''
+a = 4
+
+b = 5
+
+d = 4%5
+
+e = b % a
+
+
